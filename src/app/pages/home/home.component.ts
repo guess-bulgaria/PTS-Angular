0bc135ed96824831d22ec09f4b1b41869b6e2742
mmihaylov-aux <71435826+mmihaylov-aux@users.noreply.github.com>
2022-05-13 13:39:45 +0300

    Show data on load

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -16,19 +16,31 @@ export class HomeComponent {
 
   ngOnInit() {
     this.uploadedFileNames = JSON.parse(sessionStorage.getItem(Config.UPLOADED_FILES_KEY) || '[]');
+    this.studentsData = JSON.parse(sessionStorage.getItem(Config.STUDENTS_DATA_STORAGE_KEY) || '{}');
+    this.values = Object.values(this.studentsData);
   }
 
   uploadedFileNames: String[] = [];
+  studentsData?: any;
+  values: any[] = [];
+
+  clear(){
+    this.studentsData = {};
+    this.uploadedFileNames = [];
+    sessionStorage.setItem(Config.STUDENTS_DATA_STORAGE_KEY, JSON.stringify(this.studentsData));
+    sessionStorage.setItem(Config.UPLOADED_FILES_KEY, JSON.stringify(this.uploadedFileNames));
+    this.values = Object.values(this.studentsData);
+  }
 
   getUserIdFromDescription(description: string): number {
     let match = [...description.matchAll(this.idRegex)];
-    if(match.length == 0) throw new Error();
+    if (match.length == 0) throw new Error();
     return +match[0][1];
   }
 
   getLectureIdFromDescription(description: string): number {
     let match = [...description.matchAll(this.idRegex)];
-    if(match.length == 0) throw new Error();
+    if (match.length == 0) throw new Error();
     return +match[1][1];
   }
 
@@ -36,8 +48,6 @@ export class HomeComponent {
     const target: DataTransfer = <DataTransfer>(event.target);
 
     for (let i = 0; i < target.files.length; i++) {
-      let studentsData: any = JSON.parse(sessionStorage.getItem(Config.STUDENTS_DATA_STORAGE_KEY) || '{}');
-
       let index;
       if ((index = this.uploadedFileNames.indexOf(target.files[i].name)) != -1) {
         this.uploadedFileNames.splice(index, 1);
@@ -51,38 +61,39 @@ export class HomeComponent {
         const ws: XLSX.WorkSheet = wb.Sheets[wb.SheetNames[0]];
         const data: any[] = XLSX.utils.sheet_to_json(ws);
         for (const row of data) {
-          try{
+          try {
             let studentId;
             if ((studentId = row['ID']) != undefined) {
-              if (studentsData[studentId] === undefined)
-                studentsData[studentId] = new StudentDataModel(studentId);
+              if (this.studentsData[studentId] === undefined)
+                this.studentsData[studentId] = new StudentDataModel(studentId);
 
-              studentsData[studentId].grade = row['Result'];
+              this.studentsData[studentId].grade = row['Result'];
             } else {
               if (row['Event context'] == 'Assignment: Качване на курсови задачи и проекти' &&
                 row['Component'] == 'File submissions' &&
                 (row['Event name'] == 'Submission created.' || row['Event name'] == 'Submission updated.')) {
                 studentId = this.getUserIdFromDescription(row['Description']);
-                if (studentsData[studentId] == undefined)
-                  studentsData[studentId] = new StudentDataModel(studentId);
+                if (this.studentsData[studentId] == undefined)
+                  this.studentsData[studentId] = new StudentDataModel(studentId);
 
-                studentsData[studentId].submitAssignment();
-              } else if(row['Event context'].startsWith('File: Лекция')){
+                this.studentsData[studentId].submitAssignment();
+              } else if (row['Event context'].startsWith('File: Лекция')) {
                 let description = row['Description'];
                 studentId = this.getUserIdFromDescription(description);
-                if (studentsData[studentId] == undefined)
-                  studentsData[studentId] = new StudentDataModel(studentId);
+                if (this.studentsData[studentId] == undefined)
+                  this.studentsData[studentId] = new StudentDataModel(studentId);
 
-                studentsData[studentId].viewLecture(this.getLectureIdFromDescription(description));
+                this.studentsData[studentId].viewLecture(this.getLectureIdFromDescription(description));
               }
             }
-          } catch (_){}
+          } catch (_) {
+          }
         }
 
-        sessionStorage.setItem(Config.STUDENTS_DATA_STORAGE_KEY, JSON.stringify(studentsData));
+        sessionStorage.setItem(Config.STUDENTS_DATA_STORAGE_KEY, JSON.stringify(this.studentsData));
         sessionStorage.setItem(Config.UPLOADED_FILES_KEY, JSON.stringify(this.uploadedFileNames));
+        this.values = Object.values(this.studentsData);
       }
-      ;
     }
   }
 }
