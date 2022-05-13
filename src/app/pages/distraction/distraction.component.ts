f53e9ef90d8e7dd6bba15744426c17ec7df42749
mmihaylov-aux <71435826+mmihaylov-aux@users.noreply.github.com>
2022-05-13 15:22:29 +0300

    Distraction implemented

--- src/app/pages/distraction/distraction.component.ts
+++ src/app/pages/distraction/distraction.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { BasePage } from "../base-page";
 
 @Component({
@@ -10,5 +10,42 @@ export class DistractionComponent extends BasePage {
   constructor() {
     super();
   }
+
+  scope?: number = 0;
+  variance?: number = 0;
+  standardDeviation?: number = 0;
+
+  ngOnInit() {
+    let numbers = ([...this.studentsData.values()])
+      .map(student => student.uploadedFiles)
+      .sort((a, b) => a - b);
+    if (numbers.length == 0) return;
+
+    this.scope = this.findScope(numbers);
+    this.variance = this.calculateVariance(numbers);
+    this.standardDeviation = this.calculateSD(this.variance);
+  }
+
+  findScope(array: number[]): number {
+    return array[array.length-1] - array[0];
+  }
+
+  calculateVariance (array: number[]) {
+    const average = this.calculateMean(array);
+
+    const squareDiffs = array.map((value) => {
+      const diff = value - average;
+      return diff * diff;
+    });
+    return this.calculateMean(squareDiffs);
+  };
+
+  calculateMean (array: number[]) {
+    return (array.reduce((sum, current) => sum + current)) / array.length;
+  };
+
+  calculateSD (variance: number) {
+    return Math.sqrt(variance);
+  };
 
 }
